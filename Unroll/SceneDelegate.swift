a60edce91e517a60d9f97e161d9cd603eeab6761
Gabriel Garcia <g.garciamartins@gmail.com>
2023-09-07 11:23:15 -0300

    fix: rootViewController must  be a navigation controller with the home view controller pushed

--- Unroll/SceneDelegate.swift
+++ Unroll/SceneDelegate.swift
@@ -19,9 +19,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController()
         let viewController = HomeViewController()
         
-        window.rootViewController = viewController
+        navigationController.pushViewController(viewController, animated: false)
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         
         self.window = window
