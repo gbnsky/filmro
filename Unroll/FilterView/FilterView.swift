54993c94dad9628f34a824579167eeefcc289d08
Gabriel Garcia <g.garciamartins@gmail.com>
2023-09-08 18:31:57 -0300

    feat(filter): view were updated to receive filter components

---
+++ Unroll/FilterView/FilterView.swift
@@ -0,0 +1,100 @@
+//
+//  FilterView.swift
+//  Unroll
+//
+//  Created by Gabriel Garcia on 07/09/23.
+//
+
+import UIKit
+
+class FilterView: UIView {
+        
+    // MARK: - UI Components
+    
+    private lazy var title: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Colors.blackOne
+        label.font = UIFont(name: Fonts.breeSerif, size: 24)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Just like Subway,\nchoose the ingredients."
+        return label
+    }()
+    
+    private lazy var genreCollectionView: GenreCollectionView = {
+        let view = GenreCollectionView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Continue", for: .normal)
+        button.setTitleColor(Colors.blackOne, for: .normal)
+        button.titleLabel?.font = UIFont(name: Fonts.kanitRegular, size: 16)
+        button.backgroundColor = Colors.orange
+        button.layer.borderColor = Colors.blackOne.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 24
+        return button
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Exposed Methods
+    
+    func setupGenreCollectionView(with genres: [Genre]?) {
+        genreCollectionView.setup(with: genres)
+    }
+}
+
+// MARK: - View Coding
+
+extension FilterView {
+    
+    private func loadView() {
+        addSubviews()
+        addConstraints()
+    }
+    
+    private func addSubviews() {
+        addSubview(title)
+        addSubview(genreCollectionView)
+        addSubview(button)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            
+            // title
+            
+            title.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            title.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            title.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            
+            // genre collection view
+            
+            genreCollectionView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 32),
+            genreCollectionView.widthAnchor.constraint(equalTo: widthAnchor),
+            genreCollectionView.heightAnchor.constraint(equalToConstant: 100),
+            
+            // button
+            
+            button.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -32),
+            button.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            button.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+}
---
+++ Unroll/FilterView/FilterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FilterViewController.swift
+//  Unroll
+//
+//  Created by Gabriel Garcia on 07/09/23.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private lazy var filterView: FilterView = {
+        let view = FilterView()
+        return view
+    }()
+    
+    // MARK: - Life cycle
+    
+    override func loadView() {
+        view = filterView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Colors.beige
+        MovieApi.shared.getMovieGenres { genres in
+            self.filterView.setupGenreCollectionView(with: genres)
+//            self.filterView.setup(with: genres)
+        }
+    }
+}
