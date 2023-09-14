eedc8fdf1966b905a60b8ca176249d5092f7f0dc
Gabriel Garcia <g.garciamartins@gmail.com>
2023-09-14 20:51:33 -0300

    feat: green color were added

--- Unroll/Constants/Colors.swift
+++ Unroll/Constants/Colors.swift
@@ -12,4 +12,5 @@ struct Colors {
     static let blackOne = UIColor(red: 0.2, green: 0.216, blue: 0.275, alpha: 1)
     static let yellow = UIColor(red: 0.996, green: 0.796, blue: 0.4, alpha: 1)
     static let orange = UIColor(red: 0.969, green: 0.639, blue: 0.455, alpha: 1)
+    static let green = UIColor(red: 0.678, green: 0.839, blue: 0.722, alpha: 1)
 }
