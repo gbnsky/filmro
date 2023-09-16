02d006d4836f453d43e10caa87478f96a7dc0d21
Gabriel Garcia <g.garciamartins@gmail.com>
2023-09-16 14:04:10 -0300

    feat(home): home ui were changed to better fit the user necessities

--- Unroll/HomeView/HomeView.swift
+++ Unroll/HomeView/HomeView.swift
@@ -39,14 +39,41 @@ class HomeView: UIView {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = Colors.blackOne
-        label.font = UIFont(name: Fonts.breeSerif, size: 40)
+        label.font = UIFont(name: Fonts.breeSerif, size: 32)
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        label.text = "A shortcut for the next streaming."
+        label.textAlignment = .center
+        label.text = "A shortcut to next streaming"
         return label
     }()
     
-    private lazy var button: UIButton = {
+    private lazy var text: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Colors.blackOne
+        label.font = UIFont(name: Fonts.kanitRegular, size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        label.text = "We'll try to spare your time before countless scrolling time to find something to watch."
+        return label
+    }()
+    
+    private lazy var settingsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Settings", for: .normal)
+        button.setTitleColor(Colors.blackOne, for: .normal)
+        button.titleLabel?.font = UIFont(name: Fonts.kanitRegular, size: 16)
+        button.backgroundColor = Colors.orange
+        button.layer.borderColor = Colors.blackOne.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 24
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var continueButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Let’s find a movie together", for: .normal)
@@ -97,9 +124,11 @@ extension HomeView {
     
     private func addSubviews() {
         addSubview(logo)
-        addSubview(mascot)
         addSubview(title)
-        addSubview(button)
+        addSubview(mascot)
+        addSubview(text)
+        addSubview(settingsButton)
+        addSubview(continueButton)
     }
     
     private func addConstraints() {
@@ -110,27 +139,43 @@ extension HomeView {
             logo.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             logo.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
             logo.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
-            logo.heightAnchor.constraint(equalToConstant: 160),
-            
-            // logo
-            
-            mascot.bottomAnchor.constraint(equalTo: title.topAnchor),
-            mascot.widthAnchor.constraint(equalTo: widthAnchor),
-            mascot.heightAnchor.constraint(equalToConstant: 300),
-            mascot.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logo.heightAnchor.constraint(equalToConstant: 50),
             
             // title
             
+            title.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 32),
             title.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
-            title.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -32),
+            title.bottomAnchor.constraint(equalTo: mascot.topAnchor, constant: -16),
             title.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
             
-            // button
+            // mascot
             
-            button.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
-            button.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -32),
-            button.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
-            button.heightAnchor.constraint(equalToConstant: 48)
+            mascot.topAnchor.constraint(equalTo: title.bottomAnchor),
+            mascot.widthAnchor.constraint(equalTo: widthAnchor),
+            mascot.heightAnchor.constraint(equalToConstant: 250),
+            mascot.centerXAnchor.constraint(equalTo: centerXAnchor),
+            mascot.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            // text
+            
+            text.topAnchor.constraint(equalTo: mascot.bottomAnchor),
+            text.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            text.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            
+            // settings button
+            
+            settingsButton.topAnchor.constraint(equalTo: text.bottomAnchor),
+            settingsButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            settingsButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            settingsButton.heightAnchor.constraint(equalToConstant: 48),
+            
+            // continue button
+            
+            continueButton.topAnchor.constraint(equalTo: settingsButton.bottomAnchor, constant: 16),
+            continueButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            continueButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -32),
+            continueButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
+            continueButton.heightAnchor.constraint(equalToConstant: 48)
             
         ])
     }
