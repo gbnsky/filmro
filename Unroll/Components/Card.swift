809a462a99c5850d1585c3eadc6475303abac30e
Gabriel Garcia <g.garciamartins@gmail.com>
2023-09-18 10:08:35 -0300

    feat: card component were structured

---
+++ Unroll/Components/Card.swift
@@ -0,0 +1,97 @@
+//
+//  Card.swift
+//  Unroll
+//
+//  Created by Gabriel Garcia on 18/09/23.
+//
+
+import UIKit
+
+class Card: UIView {
+    
+    // MARK: - UI Components
+    
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        view.isLayoutMarginsRelativeArrangement = true
+        view.axis = .vertical
+        view.spacing = 16
+        view.alignment = .fill
+        view.distribution = .fill
+        view.backgroundColor = Colors.green
+        view.layer.cornerRadius = 16
+        view.layer.borderWidth = 1
+        view.layer.borderColor = Colors.blackOne.cgColor
+        return view
+    }()
+    
+    private lazy var cardTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Colors.blackOne
+        label.font = UIFont(name: Fonts.breeSerif, size: 24)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Description".localized()
+        return label
+    }()
+    
+    private lazy var cardDescription: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = Colors.blackOne
+        label.font = UIFont(name: Fonts.kanitRegular, size: 16)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Exposed Methods
+    
+    func setup(title: String, description: String, backgroundColor: UIColor) {
+        cardTitle.text = title
+        cardDescription.text = description
+        stackView.backgroundColor = backgroundColor
+    }
+}
+
+// MARK: - View Coding
+
+extension Card {
+    
+    private func loadView() {
+        addSubviews()
+        addConstraints()
+    }
+    
+    private func addSubviews() {
+        stackView.addArrangedSubview(cardTitle)
+        stackView.addArrangedSubview(cardDescription)
+        addSubview(stackView)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            
+            // stack view
+            
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor),
+            
+        ])
+    }
+}
